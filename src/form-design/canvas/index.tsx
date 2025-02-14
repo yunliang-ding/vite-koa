1cd50bc01fa572e4b1c18f9182982dd64d63d200
yunliang <804408086@qq.com>
2025-02-14 15:25:51 +0800

    fixed

--- src/form-design/canvas/index.tsx
+++ src/form-design/canvas/index.tsx
@@ -1,6 +1,7 @@
 import store from "../store";
 import Designer from "./design";
 import { getEsModuleString } from "@/components/transcoder";
+import { Select, Space } from "antd";
 
 export default () => {
   const snap = store.useSnapshot();
@@ -16,6 +17,26 @@ export default () => {
   });
   return (
     <div className="canvas">
+      <div className="canvas-headers">
+        <Space>
+          <span>当前主题</span>
+          <Select
+            style={{ width: 160 }}
+            size="small"
+            defaultValue={"antd"}
+            options={[
+              {
+                label: "Ant Design",
+                value: "antd",
+              },
+              {
+                label: "SHINEOUT",
+                value: "shineout",
+              },
+            ]}
+          />
+        </Space>
+      </div>
       <Designer source={source} />
     </div>
   );
